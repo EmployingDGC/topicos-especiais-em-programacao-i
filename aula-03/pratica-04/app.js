d717ae47d05caccc780bee2d542dbec74239baef
Davi GC <89748128+MayaneFPViana@users.noreply.github.com>
2022-09-23 19:55:12 -0300

    aula 03

--- aula-03/pratica-04/app.js
+++ aula-03/pratica-04/app.js
@@ -1,14 +1,30 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		aplicar_destaque: true,
+		aplicar_css_4: "false",
+		input_css_4: "",
+		input_css_5: {
+			width: "150px",
+			height: "150px"
+		},
+		cor_5: "",
+		input_css: "",
+	},
+	watch: {
+		
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => {
+				this.aplicar_destaque = !this.aplicar_destaque
+			}, 3000)
 		},
 		iniciarProgresso() {
 
+		},
+		f_aplicar_css_4(e) {
+			this.aplicar_css_4 = e.target.value == 'true'
 		}
 	}
 })
