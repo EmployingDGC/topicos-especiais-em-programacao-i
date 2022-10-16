84651645f522e677ccb4fcdc7a65ade3f873f963
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-10-16 15:34:12 -0300

    componente desafio 2

---
+++ componente-desafio2/vue-cli/project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <NavigationDrawer/>
+    <AppBar/>
+    <Content/>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import NavigationDrawer from './components/NavigationDrawer.vue';
+import AppBar from './components/AppBar.vue';
+import Content from './components/Content.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppBar,
+    NavigationDrawer,
+    Content,
+    Footer
+  }
+}
+</script>
---
+++ componente-desafio2/vue-cli/project/src/components/NavigationDrawer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-navigation-drawer app>
+    <ToolBar/>
+    <v-divider/>
+    <Menu/>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import ToolBar from './ToolBar.vue';
+import Menu from './Menu.vue';
+
+export default {
+  name: "NavigationDrawer",
+  components: {
+    ToolBar,
+    Menu
+  }
+}
+</script>
