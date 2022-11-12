5f0de72bf0c3104abcc14465a0fa7d748618390a
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-11-12 00:10:19 -0300

    adicionando SearchBar, ContainerNews e IndicationToFollow

--- vue-cli/intriga/src/components/Side.vue
+++ vue-cli/intriga/src/components/Side.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="side">
-        a
+        <SearchBar />
+        <ContainerNews />
+        <IndicationToFollow :users="users"/>
     </div>
 </template>
 
 <script>
+import IndicationToFollow from "./IndicationToFollow.vue"
+import SearchBar from "./SearchBar.vue"
+import ContainerNews from "./ContainerNews.vue"
+
 export default {
     name: "Side",
+    props: ["users"],
     components: {
-        
+        SearchBar,
+        ContainerNews,
+        IndicationToFollow
     },
     data() {
         return {
