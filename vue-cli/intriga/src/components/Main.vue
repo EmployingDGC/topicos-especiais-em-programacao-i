b58adada58dcddc3fdd52f107e43df0219246a97
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-12-02 22:33:00 -0300

    adicionando data de última edição para as postagens

--- vue-cli/intriga/src/components/Main.vue
+++ vue-cli/intriga/src/components/Main.vue
@@ -9,6 +9,22 @@
                 :post="p"
             />
         </div>
+        <div
+            v-if="edited_post"
+            class="edited-post"
+        >
+            <div class="content">
+                <textarea
+                    v-model.trim="edited_post.content"
+                    placeholder="O que está acontecendo?"
+                    maxlength="280"
+                    wrap="hard"
+                ></textarea>
+                <button
+                    @click="edit_post"
+                >Salvar</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,6 +34,7 @@ import Post from "./Post.vue"
 
 import onAddPost from "../events/onAddPost"
 import onDelPost from "../events/onDelPost"
+import onEditPost from "../events/onEditPost"
 
 export default {
     name: "Main",
@@ -26,13 +43,23 @@ export default {
         Intrigar,
         Post
     },
+    watch: {
+        posts: {
+            handler(new_value, old_value) {
+                new_value
+                old_value
+            },
+            deep: true
+        }
+    },
     data() {
         return {
             posts: [
-                {id: 1, user_id: 3, content: "Partiu Programar #VueJS", qtt_like: 5, qtt_reintrig: 0, date: new Date("2022-01-01 17:55:13")},
-                {id: 2, user_id: 6, content: "Partiu Programar #VueJS", qtt_like: 10, qtt_reintrig: 3, date: new Date("2022-02-02 19:23:27")},
-                {id: 3, user_id: 5, content: "Partiu Programar #VueJS", qtt_like: 15, qtt_reintrig: 1, date: new Date("2022-03-03 10:15:33")},
-            ]
+                {id: 1, user_id: 3, content: "Partiu Programar #VueJS", qtt_like: 5, qtt_reintrig: 0, date: new Date("2022-01-01 17:55:13"), date_edit: null},
+                {id: 2, user_id: 6, content: "Partiu Programar #VueJS", qtt_like: 10, qtt_reintrig: 3, date: new Date("2022-02-02 19:23:27"), date_edit: null},
+                {id: 3, user_id: 5, content: "Partiu Programar #VueJS", qtt_like: 15, qtt_reintrig: 1, date: new Date("2022-03-03 10:15:33"), date_edit: null},
+            ],
+            edited_post: null
         }
     },
     methods: {
@@ -68,6 +95,12 @@ export default {
             }
 
             return null
+        },
+        edit_post() {
+            if (this.edited_post.content) {
+                this.edited_post.date_edit = new Date()
+                this.edited_post = null
+            }
         }
     },
     created() {
@@ -93,10 +126,28 @@ export default {
                 }
             }
         })
+        
+        onEditPost.$on("edit-post", (post) => {
+            const user_id = this.get_logged_user().id
+
+            if (user_id != post.user_id) {
+                return
+            }
+
+            for (let i = 0; i < this.posts.length; i += 1) {
+                const p = this.posts[i]
+                
+                if (p.id == post.id) {
+                    this.edited_post = p
+                    return
+                }
+            }
+        })
     },
     destroyed() {
         onAddPost.$off("add-post")
         onDelPost.$off("del-post")
+        onEditPost.$off("edit-post")
     }
 }
 </script>
@@ -109,5 +160,56 @@ export default {
 
     .container-posts {
         margin-top: 10px;
+    }
+
+    .edited-post {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #000000ba;
+        z-index: 500;
+    }
+
+    .edited-post .content {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .edited-post .content textarea {
+        height: 500px;
+        width: 500px;
+        resize: none;
+        outline: none;
+        border: none;
+        background-color: var(--color-background-side);
+        padding: 12px;
+        border-radius: 20px;
+    }
+
+    .edited-post .content button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 90px;
+        height: 36px;
+        outline: none;
+        border: none;
+        border-radius: 40px;
+        padding: 16px;
+        background-color: var(--color-background-button-blue);
+        color: var(--color-text-white);
+        font-weight: 900;
+        margin-top: 5px;
+        cursor: pointer;
+        font-size: 1.1rem;
+    }
+
+    .edited-post .content button:hover {
+        background-color: var(--color-background-button-hover-blue);
     }
 </style>
--- vue-cli/intriga/src/components/Post.vue
+++ vue-cli/intriga/src/components/Post.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="post">
-        <span
-            class="trash-can"
-            @click="del_post"
-        ><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"/></svg></span>
+        <div class="container-options">
+            <span
+                class="icon-edit"
+                @click="edit_post"
+            ><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="485.219px" height="485.22px" viewBox="0 0 485.219 485.22" style="enable-background:new 0 0 485.219 485.22;" xml:space="preserve"> <g><path d="M467.476,146.438l-21.445,21.455L317.35,39.23l21.445-21.457c23.689-23.692,62.104-23.692,85.795,0l42.886,42.897 C491.133,84.349,491.133,122.748,467.476,146.438z M167.233,403.748c-5.922,5.922-5.922,15.513,0,21.436 c5.925,5.955,15.521,5.955,21.443,0L424.59,189.335l-21.469-21.457L167.233,403.748z M60,296.54c-5.925,5.927-5.925,15.514,0,21.44 c5.922,5.923,15.518,5.923,21.443,0L317.35,82.113L295.914,60.67L60,296.54z M338.767,103.54L102.881,339.421 c-11.845,11.822-11.815,31.041,0,42.886c11.85,11.846,31.038,11.901,42.914-0.032l235.886-235.837L338.767,103.54z M145.734,446.572c-7.253-7.262-10.749-16.465-12.05-25.948c-3.083,0.476-6.188,0.919-9.36,0.919 c-16.202,0-31.419-6.333-42.881-17.795c-11.462-11.491-17.77-26.687-17.77-42.887c0-2.954,0.443-5.833,0.859-8.703 c-9.803-1.335-18.864-5.629-25.972-12.737c-0.682-0.677-0.917-1.596-1.538-2.338L0,485.216l147.748-36.986 C147.097,447.637,146.36,447.193,145.734,446.572z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></span>
+            <span
+                class="icon-trash-can"
+                @click="del_post"
+            ><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"/></svg></span>
+        </div>
         <User :user="user" :enabled_infos="true" />
         <p>{{ post.content }}</p>
-        <span class="date">Postado em {{ _format_date(post.date) }}</span>
+        <div class="container-date">
+            <span class="date">Postado em {{ _format_date(post.date) }}</span>
+            <span v-if="post.date_edit" class="date">Editado em {{ _format_date(post.date_edit) }}</span>
+        </div>
         <div class="interact">
             <a href="#">
                 <span><svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"><g><path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path></g></svg></span>
@@ -33,6 +42,7 @@ import User from "./User.vue"
 
 import { format_date } from "../utils/date"
 import onDelPost from "../events/onDelPost"
+import onEditPost from "../events/onEditPost"
 
 export default {
     name: "Post",
@@ -51,6 +61,9 @@ export default {
         },
         del_post() {
             onDelPost.$emit("del-post", this.post)
+        },
+        edit_post() {
+            onEditPost.$emit("edit-post", this.post)
         }
     }
 }
@@ -94,31 +107,59 @@ export default {
         margin-bottom: 10px;
     }
 
+    .container-date {
+        display: flex;
+        justify-content: space-between;
+    }
+
     .date {
         font-size: .8rem;
     }
 
-    .trash-can {
+    .container-options {
         position: absolute;
         right: 5px;
         top: 5px;
-        width: 30px;
-        height: 30px;
-        border-radius: 50%;
         display: flex;
         justify-content: center;
         align-items: center;
     }
 
-    .trash-can:hover {
+    .container-options > * {
+        border-radius: 50%;
+    }
+    
+    .container-options > * svg {
+        padding: 2px;
+        width: 20px;
+        height: 20px;
+    }
+
+    .icon-trash-can {
+        margin-left: 5px;
+    }
+
+    .icon-trash-can:hover {
         background-color: var(--color-background-button-hover-white);
     }
 
-    .trash-can:hover svg {
+    .icon-trash-can:hover svg {
         fill: red;
     }
 
-    .trash-can svg {
+    .icon-trash-can svg {
+        fill: var(--color-text-gray);
+    }
+
+    .icon-edit:hover {
+        background-color: var(--color-background-button-hover-white);
+    }
+
+    .icon-edit:hover svg {
+        fill: green;
+    }
+
+    .icon-edit svg {
         fill: var(--color-text-gray);
     }
 </style>
