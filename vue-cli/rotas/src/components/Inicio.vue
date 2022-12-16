53be6456f726001387a8e90e7cdd094b4f088f4b
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-12-15 21:28:35 -0300

    rotas - configuração inicial

---
+++ vue-cli/rotas/src/components/Inicio.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="inicio">
+        <h2>Início</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Inicio",
+    components: {
+        
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ vue-cli/rotas/src/components/usuarios/Usuario.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="usuario">
+        <h2>Usuário</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Usuario",
+    components: {
+        
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
+
